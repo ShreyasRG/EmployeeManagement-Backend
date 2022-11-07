0f32ef4a6e71e3cbadec1fa06c828925e5982109
ShreyasRG <shreyasgowda180@gmail.com>
2022-11-07 21:58:22 +0530

    deleted

--- project/src/main/java/com/project/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.project.dao;
-
-import com.project.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-
-}
